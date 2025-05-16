ee055a45da67f36609c6cf2005388a518e7c3748
Jpham2005 <tuanpham10052005@gmail.com>
2025-05-16 16:36:57 -0400

    create student class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,10 @@
-public class Student {
+public class Student extends Person {
+
+    public Student(String firstName, String lastName, String title, int totalHours) {
+        super(firstName, lastName, title, totalHours);
+    }
+    @Override
+    public  int getParticipatingHours(int courseHours) {
+        return courseHours;
+    }
 }
