a31e96dcca6ec630e2c4ae8cfad1f07c708468aa
Jpham2005 <tuanpham10052005@gmail.com>
2025-05-16 21:13:23 -0400

    develope student in main

--- src/Main.java
+++ src/Main.java
@@ -19,9 +19,9 @@ public class Main {
                 "Student Brad Pitt Chemistry 6",
                 "Student Will Smith Chemistry 6"
         };
-
+        Person[] students = new Person[data.length];
         Person[] people = new Person[data.length]; // The size of the array doesn't really matter, this array is used to
-                                                   // hold all of the different people
+        int countStudent = 0;                                           // hold all of the different people
         int[] hours = new int[data.length]; // This array will hold the number of hours for each person
         int count = 0; // This will be used to keep track of the index of the people array
 
@@ -33,7 +33,9 @@ public class Main {
             String firstName = parts[1];
             String lastName = parts[2];
             String course = parts[3];
+            int courseHours = Integer.parseInt(parts[4]);
 
+            boolean isFound = false;
             // Initializes the person before assiging its role, first name, last name, and
             // hours worked
             Person p;
@@ -44,6 +46,24 @@ public class Main {
                 p = new TA(firstName, lastName);
             } else {
                 p = new Student(firstName, lastName, course, 0);
+            }
+
+
+
+            // Tuan
+            if (role.equalsIgnoreCase("Student")) {
+                for (int i = 0; i < countStudent; i++) {
+                    if(students[i] == null && students[i].getFirstName().equalsIgnoreCase(firstName) && students[i].getLastName().equalsIgnoreCase(lastName)) {
+                        students[i].setTotalHours(students[i].getTotalHours() + students[i].getParticipatingHours(courseHours));
+                        isFound = true;
+                    }
+                }
+                if (!isFound) {
+                    Person student = new Student(firstName, lastName, course, 0);
+                    students[countStudent] = student;
+                    students[countStudent].setTotalHours(students[countStudent].getParticipatingHours(courseHours));
+                    countStudent++;
+                }
             }
 
             
